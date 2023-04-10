2eb1e5ba5f87dcd18b11de6305e2e0de3824069e
TruongDang23 <quantruong050123@gmail.com>
2023-04-10 21:56:16 +0700

    version 10/4

--- server.js
+++ server.js
@@ -44,6 +44,53 @@ app.post('/login',(req,res)=>{
     })
 })
 
+app.post('/tracking',(req,res)=>{
+    const query="with Q1(ID) as (select ID_patient from list_tracking where `User`= ?)\
+            select information.ID_patient,Name,Age,Address from Q1 inner join information\
+            on Q1.ID=information.ID_patient"
+    db.query(query,[req.body.Username],(err,data)=>{
+        if(err){
+            return res.json("Error")
+        }
+        return res.json(data)
+    })
+})
+
+app.post('/estimate',(req,res)=>{
+    const query="select * from estimate where `ID_patient` = ?"
+    db.query(query,[req.body.ID],(err,data)=>{
+        if(err){
+            return res.json("Error")
+        }
+        return res.json((data))
+    })
+})
+
+app.post('/find',(req,res)=>{
+    const query="select * from information where `ID_patient`=?"
+    db.query(query,[req.body.ID],(err,data)=>{
+        if(err){
+            return res.json("Error")
+        }
+        return res.json(data)
+    })
+})
+
+app.post('/addtracking',(req,res)=>{
+    const query="insert into list_tracking (`User`,`ID_patient`) values(?)"
+    const values=[
+        req.body.Username,
+        req.body.ID_patient,
+    ]
+    db.query(query,[values],(err,data)=>{
+        if(err){
+            return res.json(err)
+        }
+        return res.json(data)
+    })
+})
+
+
 app.listen(3000,()=>{
     console.log("Listening at port 3000")
 })
