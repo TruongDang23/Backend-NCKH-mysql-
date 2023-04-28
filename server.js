7e3cd44aa7f693e568176f3b6f8679089200b036
TruongDang23 <quantruong050123@gmail.com>
2023-04-28 18:26:50 +0700

    28/4/23

--- server.js
+++ server.js
@@ -7,36 +7,41 @@ const app=express()
 app.use(cors())
 app.use(express.json())
 
+let i = 0
+
 const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"account",
+    host:"103.130.211.150",
+    port:"10039",
+    user:"nckh",
+    password:"nckhfit",
+    database:"mydb",
 })
 
 app.post('/create',(req,res)=>{
-    const query="insert into login (`Username`,`Email`,`Pass`) values (?)"
+    const query="insert into logins (`Username`,`Email`,`Pass`,`ID`) values (?)"
     const values=[
         req.body.Username,
         req.body.Email,
         req.body.Pass,
+        i,
     ]
     db.query(query,[values],(err,data)=>{
         if(err){
             return res.json("Error")
         }
+        i=i+1
         return res.json(data)
     })
 })
 
 app.post('/login',(req,res)=>{
-    const query="select * from login where `Username` = ? and `Pass` = ?"
+    const query="select * from logins where `Username` = ? and `Pass` = ?"
     db.query(query,[req.body.Username,req.body.Pass],(err,data)=>{
         if(err){
             return res.json("Error")
         }
         if(data.length>0){
-            return res.json("Success")
+            return res.json(data)
         }
         else{
             return res.json("Fail")
@@ -45,10 +50,9 @@ app.post('/login',(req,res)=>{
 })
 
 app.post('/tracking',(req,res)=>{
-    const query="with Q1(ID) as (select ID_patient from list_tracking where `User`= ?)\
-            select information.ID_patient,Name,Age,Address from Q1 inner join information\
-            on Q1.ID=information.ID_patient"
-    db.query(query,[req.body.Username],(err,data)=>{
+    const query="with Q1(ID) as (select ID_patient from mydb.list_tracking where `User`= ?) select information.ID_patient,Name,Age,Address from mydb.information inner join Q1 on Q1.ID=information.ID_patient"
+            
+    db.query(query,[req.body.ID],(err,data)=>{
         if(err){
             return res.json("Error")
         }
@@ -89,7 +93,7 @@ app.post('/find',(req,res)=>{
 app.post('/addtracking',(req,res)=>{
     const query="insert into list_tracking (`User`,`ID_patient`) values(?)"
     const values=[
-        req.body.Username,
+        req.body.ID,
         req.body.ID_patient,
     ]
     db.query(query,[values],(err,data)=>{
@@ -100,8 +104,4 @@ app.post('/addtracking',(req,res)=>{
     })
 })
 
-
-app.listen(3000,()=>{
-    console.log("Listening at port 3000")
-})
-
+app.listen(3000) //Open port 3000
